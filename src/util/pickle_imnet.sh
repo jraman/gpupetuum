edc5aea567a8d56b5a0f5287bb739cd1594ab1d9
Jayanth Raman <raman.jayanth@gmail.com>
2015-08-28 20:36:11 -0700

    command line args for pickle_llc

--- src/util/pickle_imnet.sh
+++ src/util/pickle_imnet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python pickle_llc.py ../imnet_data/imnet_0.bin ../imnet_data/0_label.txt ../imnet_data/imnet_0.pkl
-
+DATADIR=../imnet_data
+python pickle_llc.py -i ${DATADIR}/imnet_0.bin -l ${DATADIR}/0_label.txt -o ${DATADIR}/imnet_0.pkl -n 21504
